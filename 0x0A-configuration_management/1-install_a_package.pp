6af96a0583ae743ccd2e57f4807619a7d72d7937
busayolawumi <busayoolawumi3@gmail.com>
2023-12-22 22:58:37 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,23 @@
-# Install Flask using pip3 with version 2.1.0
-package { 'Flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    require  => Package['python3-pip'],
+# Install Python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# Install pip
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask 2.1.0
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
